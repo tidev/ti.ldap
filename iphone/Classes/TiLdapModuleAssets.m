d17e1d8a3f19be0ff36d0857c995f75a76db657f
Jeff English <jeff@utmostlogic.com>
2012-10-01 11:22:56 -0500

    [MOD-929] Initial project creation with single multiplatform build.py support

---
+++ iphone/Classes/TiLdapModuleAssets.m
@@ -0,0 +1,15 @@
+/**
+ * This is a generated file. Do not edit or your changes will be lost
+ */
+#import "TiLdapModuleAssets.h"
+
+extern NSData* filterDataInRange(NSData* thedata, NSRange range);
+
+@implementation TiLdapModuleAssets
+
+- (NSData*) moduleAsset
+{
+	return nil;
+}
+
+@end
